a450f6d65b23267bf7f265a5e17f906f791bf450
Valod Voskanyan <valodjan13@gmail.com>
2025-05-20 02:10:06 +0400

    C++ for loop

--- Classwork/erkrord.cpp
+++ Classwork/erkrord.cpp
@@ -1,45 +1,28 @@
 #include <iostream>
 #include <string>
 
-struct Student {
-std::string name;
-int grade;
-};
 
 int main() {
-	Student student;
-	student.name = "";
-	student.grade = 0;
-	
-	std::cout << "Input your name" << std::endl;
-	std::cin >> student.name;
-	
-		std::cout << "Please Input your Grade " << std::endl;
-	 
-	std::cin >> student.grade;
-	if ( student.grade < 0 ) {
-		std::cout << "Input positive number" << std::endl;
+for (int i = 0; i < 3 ; i++ ) {
+std::string username;
+std::string password;
+	username = "Student";
+	password = "Student";
+
+std::cout << "Input your username :" << std::endl;
+std::cin >> username;
+if ( username == "Student" ) {
+	  }			
+std::cout << "Input your password :" << std::endl;
+std::cin >> password;
+	if ( password == "Student" ) {
+		std::cout << "Login Succesfull" << std::endl;
+			break; }
+		else {
+			std::cout << "Wrong Username or Password" << std::endl;
+
 }
 
-else if ( student.grade <= 40 ) {
-	std::cout << "Your grade is D " << std::endl;
 }
-
-else if ( student.grade <= 60 ) {
-	std::cout << "Your grade is C " << std::endl;
-}
-
-else if ( student.grade <= 80 ) {
-	std::cout << "Your grade is B " <<  std::endl;
-}
-
-else if ( student.grade <= 100 ) {
-	std::cout << "Your grade is A " << std::endl;
-} 
-
-else { 
-	std::cout << "Grade is too high (0-100)" << std::endl;
-	}
 return 0;	
-};
-
+}
